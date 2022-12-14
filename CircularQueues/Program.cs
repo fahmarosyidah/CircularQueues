a19b2384f0a052324100b90c090eefeb4ddbae5c
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-12-14 16:34:37 +0700

    deklarasi variabel dan array

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -4,6 +4,9 @@ namespace CircularQueues
 {
     class Queues
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
