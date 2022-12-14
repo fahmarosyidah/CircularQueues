5771b1b4e73f95989d518f6ecace34e364f9fc1e
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-12-14 17:21:35 +0700

    menambahkan switch case

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -124,7 +124,38 @@ namespace CircularQueues
                     Console.Write("\nEnter your choice (1-4): ");
                     ch = Convert.ToChar(Console.ReadLine());
                     Console.WriteLine();
-
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                Console.WriteLine("Enter a number: ");
+                                int num = Convert.ToInt32(System.Console.ReadLine());
+                                Console.WriteLine();
+                                q.insert(num);
+                            }
+                            break;
+                        case '2':
+                            {
+                                q.remove();
+                            }
+                            break;
+                        case '3':
+                            {
+                                q.display();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid Option!!");
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Check for the values entered");
                 }
             }
         }
